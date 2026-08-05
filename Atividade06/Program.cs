95b99c04d1f8ffa2aa5a730fe2aaf453ed6d9ecd
KCastro-r <k.fernanda_07@yahoo.com.br>
2026-08-05 18:06:37 -0300

    Resolução Atividade06

---
+++ Atividade06/Program.cs
@@ -0,0 +1,10 @@
+﻿/*6. Contagem crescente
+Mostre os números de 1 até 100.*/
+
+Console.WriteLine("-----------CONTAGEM CRESCENTE -----------");
+
+int i;
+for(i=1; i<=100;i++)
+{
+    Console.WriteLine(i);
+}
