2114ff34dc0122d4b419e0e2f76e9edd4c915c53
KCastro-r <k.fernanda_07@yahoo.com.br>
2026-08-05 18:27:09 -0300

    Resolução Atividade 09

---
+++ Atividade09/Program.cs
@@ -0,0 +1,14 @@
+﻿/*9. Soma dos números pares
+Calcule a soma de todos os números pares entre 1 e 100.*/
+
+Console.WriteLine ("------------ SOMA DOS PARES------------");
+int somaPares=0;
+int i;
+for (i=1; i<=100; i++)
+{
+    if (i%2==0)
+    {
+        somaPares=somaPares+i;
+    }
+}
+ Console.WriteLine($"a soma de todos os números pares entre 1 e 100 é: {somaPares}");
