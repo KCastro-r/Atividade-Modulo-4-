eb3037ae4a8d6e8f2e5353c660f99a90dc40c88b
KCastro-r <k.fernanda_07@yahoo.com.br>
2026-08-05 19:11:21 -0300

    Resolução Atividade 10

---
+++ Atividade10/Program.cs
@@ -0,0 +1,34 @@
+﻿/*10. Login simples
+Solicite usuário e senha.
+Enquanto a senha estiver incorreta, peça novamente.*/
+
+Console.WriteLine("------------ LOGIN ------------");
+string? senha;
+string? usuario;
+string usuarioCorreto = "kenia";
+string senhaCorreta = "1234";
+
+do
+{
+    Console.Write("Digite o usuário: ");
+    usuario = Console.ReadLine();
+
+    Console.Write("Digite a senha: ");
+    senha = Console.ReadLine();
+
+    if (usuario != usuarioCorreto && senha != senhaCorreta)
+    {
+        Console.WriteLine("Usuário e senha incorretos!");
+    }
+    else if (usuario == usuarioCorreto && senha != senhaCorreta)
+    {
+        Console.WriteLine("Senha está incorreta!");
+    }
+    else if (usuario != usuarioCorreto && senha == senhaCorreta)
+    {
+        Console.WriteLine("Usuário está incorreto!");
+    }
+
+} while (usuario != usuarioCorreto || senha != senhaCorreta);
+
+Console.WriteLine("Login realizado com sucesso!");
