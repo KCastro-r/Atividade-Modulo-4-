9e060395d35cf9295b46379ffbbc4b8c86d2689a
KCastro-r <k.fernanda_07@yahoo.com.br>
2026-08-05 17:19:24 -0300

    Resolução Atividade 03

---
+++ Atividade03/Program.cs
@@ -0,0 +1,30 @@
+﻿/*3. Área de um retângulo
+Solicite:
+● Base
+● Altura
+Calcule:
+● Área
+● Perímetro*/
+
+Console.WriteLine("-------Área de um retângulo-------");
+
+double baseRetangulo;
+double altura;
+double area;
+double perimetro;
+
+Console.WriteLine("Informe o valor da base:");
+while(!double.TryParse(Console.ReadLine(), out baseRetangulo))
+{
+    Console.WriteLine("Digite um número válido:"); 
+}
+
+Console.WriteLine("Informe o valor da altura:");
+while(!double.TryParse(Console.ReadLine(), out altura ))
+{
+    Console.WriteLine("Digite um número válido:"); 
+}
+area = baseRetangulo * altura;
+perimetro=((baseRetangulo*2)+(altura*2));
+
+Console.WriteLine($"A área do Retângulo é {area} e o seu Perímetro é {perimetro}.");
