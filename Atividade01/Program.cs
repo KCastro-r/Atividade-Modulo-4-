12a5c9cb7f7f6156feeeb496bca471840333f010
KCastro-r <k.fernanda_07@yahoo.com.br>
2026-08-05 16:54:28 -0300

    Resolução Atividade 01

---
+++ Atividade01/Program.cs
@@ -0,0 +1,27 @@
+﻿/*1. Soma de dois números
+Solicite dois números inteiros e exiba a soma.
+Exemplo
+Entrada:
+10
+15
+Saída:
+A soma é 25.
+*/
+
+Console.WriteLine("-------- A SOMA DE DOIS NÚMEROS --------");
+int num1;
+int num2;
+int soma;
+Console.WriteLine("Informe o primeiro número inteiro:");
+while (!int.TryParse(Console.ReadLine(), out num1))
+{
+    Console.WriteLine("Digite um número inteiro válido:");
+}
+Console.WriteLine("Informe o segundo número inteiro:");
+while (!int.TryParse(Console.ReadLine(), out num2))
+{
+    Console.WriteLine("Digite um número inteiro válido:");
+}
+
+soma=num1+num2;
+Console.WriteLine($" A soma entre {num1} e {num2} é: {soma}");
