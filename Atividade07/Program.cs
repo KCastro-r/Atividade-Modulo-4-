bac2de26938c36f90bf9cf6969833fa6cba0afc6
KCastro-r <k.fernanda_07@yahoo.com.br>
2026-08-05 18:09:19 -0300

    Resolução Atividade07

---
+++ Atividade07/Program.cs
@@ -0,0 +1,10 @@
+﻿/*7. Contagem regressiva
+Mostre os números de 50 até 1.*/
+
+Console.WriteLine("-----------CONTAGEM REGRESSIVA -----------");
+
+int i;
+for(i=50; i>=1;i--)
+{
+    Console.WriteLine(i);
+}
