42de3bf797b135719365dc1773ce1b06960c632c
KCastro-r <k.fernanda_07@yahoo.com.br>
2026-08-05 17:06:23 -0300

    Resolução de Atividade 02

---
+++ Atividade02/Program.cs
@@ -0,0 +1,21 @@
+﻿/*2. Antecessor e sucessor
+Leia um número inteiro e mostre:
+● Antecessor
+● Número informado
+● Sucessor*/
+
+
+
+Console.WriteLine("------ Antecessor e sucessor ------");
+
+int num;
+int antecessor;
+int sucessor;
+Console.WriteLine("Informe um número inteiro:");
+while(!int.TryParse(Console.ReadLine(), out num))
+{
+  Console.WriteLine("Digite um número inteiro válido:");  
+}
+antecessor=num-1;
+sucessor=num+1;
+Console.WriteLine($"Para o número:{num}, o Antecessor é {antecessor} e o sucessor é {sucessor}");
