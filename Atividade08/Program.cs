238235f5dcb4a4712500126173e54cba3be7e7d7
KCastro-r <k.fernanda_07@yahoo.com.br>
2026-08-05 18:19:29 -0300

    Resolução Atividade 08

---
+++ Atividade08/Program.cs
@@ -0,0 +1,19 @@
+﻿/*8. Tabuada
+Solicite um número e exiba sua tabuada de 1 a 10.*/
+
+Console.WriteLine("------------ TABUADA------------");
+
+int num;
+int i;
+int resultado;
+Console.WriteLine("Informe o número para o calculo da Tabuada:");
+while(!int.TryParse(Console.ReadLine(), out num))
+{
+    Console.WriteLine("Valor informado inválido. Favor informar um número inteiro:");
+}
+Console.WriteLine($"A tabuada é:");
+for (i=1; i<=10; i++)
+{
+    resultado=num*i;
+    Console.WriteLine($"{num} x {i} = {resultado}");
+}
