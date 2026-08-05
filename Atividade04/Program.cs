1360986c861b0e2fad2a0021a224781484b60c4c
KCastro-r <k.fernanda_07@yahoo.com.br>
2026-08-05 17:32:49 -0300

    Resolução Atividade 04

---
+++ Atividade04/Program.cs
@@ -0,0 +1,24 @@
+﻿/*4. Conversor de moedas
+Leia um valor em reais e a cotação do dólar.
+Mostre quanto esse valor representa em dólares.*/
+
+Console.WriteLine("------- CONVERSOR DE MOEDAS ------- ");
+double valorReais;
+double conversao;
+double cotacaoDollar;
+
+Console.WriteLine("Digite o valor em reais que deseja converter:");
+while(!double.TryParse(Console.ReadLine(), out valorReais))
+{
+     Console.WriteLine("Digite um valor em reais válido:"); 
+}
+
+Console.WriteLine("Digite o valor da cotação do dólar DIA:");
+while(!double.TryParse(Console.ReadLine(), out cotacaoDollar))
+{
+   Console.WriteLine("Digite o valor da cotação dolar válido:");  
+}
+
+conversao=cotacaoDollar*valorReais;
+
+Console.WriteLine($"A conversão de R$ {valorReais} em dólares é: US$ {conversao}.");
