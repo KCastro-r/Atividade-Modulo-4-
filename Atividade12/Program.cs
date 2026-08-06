6093d7270e38ee467b90d49e220b54e4ab4f3cda
KCastro-r <k.fernanda_07@yahoo.com.br>
2026-08-06 10:53:12 -0300

    Resolução Atividade 12

---
+++ Atividade12/Program.cs
@@ -0,0 +1,21 @@
+﻿/*12. Contador de caracteres
+Leia um nome e informe quantos caracteres ele possui.*/
+
+Console.WriteLine("-------------- CONTADOR DE CARACTERES -------------- ");
+
+string? nome;
+int quantidadeCaracteres;
+Console.WriteLine("Informe o seu nome completo:");
+nome=Console.ReadLine();
+if (nome!="")
+{
+    nome = nome.Trim().Replace(" ", "");
+    quantidadeCaracteres = nome.Length;
+    Console.WriteLine($"A quantidade de caracteres do nome informado: {quantidadeCaracteres}");
+}
+else
+{
+    Console.WriteLine("Nenhum nome foi informado. Digite um valor válido.");
+}
+
+
