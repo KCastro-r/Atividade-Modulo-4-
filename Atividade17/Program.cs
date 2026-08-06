9973799d89ab60f5b4cade6e6e6d05a99a069a51
KCastro-r <k.fernanda_07@yahoo.com.br>
2026-08-06 15:05:52 -0300

    Resolução Atividade 17

---
+++ Atividade17/Program.cs
@@ -0,0 +1,20 @@
+﻿/*17. Ordem inversa
+Leia 10 números e exiba-os na ordem inversa.*/
+
+Console.WriteLine("------------- ORDEM INVERSA ------------- ");
+int[] numeros = new int[10];
+int i;
+
+for (i=0; i<10; i++)
+{
+     Console.WriteLine($"Digite o {i+1}º número:");
+
+    while (!int.TryParse(Console.ReadLine(), out numeros[i]))
+    {
+        Console.WriteLine("Entrada inválida. Digite um número inteiro:");
+    }
+}
+for (i=9; i>=0; i--)
+{
+    Console.WriteLine($"O {i+1}º número digitado foi: {numeros[i]}");
+}
