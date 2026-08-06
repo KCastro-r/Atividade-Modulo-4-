bf331c87f457b8aaa3c91e7b6320e3f50cee3aaf
KCastro-r <k.fernanda_07@yahoo.com.br>
2026-08-06 14:39:33 -0300

    Resolução Atividade 15

---
+++ Atividade15/Program.cs
@@ -0,0 +1,22 @@
+﻿/*15. Média de um vetor
+Leia 8 notas e calcule a média.*/
+
+Console.WriteLine("------------ Média DE UM VETOR ------------");
+
+double [] notas = new double[8];
+int i;
+double media;
+double somaNotas=0;
+
+for (i = 0; i < 8; i++)
+{
+    Console.WriteLine($"Digite a {i + 1}º nota:");
+
+    while (!double.TryParse(Console.ReadLine(), out notas[i]))
+    {
+        Console.WriteLine("Entrada inválida. Digite uma nota válida:");
+    }
+    somaNotas=somaNotas+notas[i];
+}
+media=somaNotas/8;
+Console.WriteLine($"A média das 8 notas é: {media}");
