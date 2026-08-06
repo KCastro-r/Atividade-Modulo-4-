30db44d82267a0ca51a07455af8f264ced6c71a7
KCastro-r <k.fernanda_07@yahoo.com.br>
2026-08-06 14:25:00 -0300

    Resolução Atividade 14

---
+++ Atividade14/Program.cs
@@ -0,0 +1,26 @@
+﻿/*14. Maior elemento
+Leia 10 números e informe:
+● Maior valor
+● Posição em que ele aparece*/
+
+Console.WriteLine("----------- MAIOR ELEMENTO ----------- ");
+
+int num;
+int i;
+int maiorNum=0;
+int maiorPos=0;
+
+for(i=1; i<=10; i++)
+{
+    Console.WriteLine($"Digite o {i}º número inteiro:");
+    while(!int.TryParse(Console.ReadLine(), out num))
+    {
+      Console.WriteLine($"Entrada Inválida. Digite um número inteiro:");  
+    }
+    if (i == 1 || maiorNum < num)
+    {
+        maiorNum = num;
+        maiorPos = i;
+    }
+}
+Console.WriteLine($" O maior valor digitado é: {maiorNum} e se encontra na posição: {maiorPos}");
