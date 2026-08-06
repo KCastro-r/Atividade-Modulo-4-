3d282e954ffca579a152b101070a50123cef6093
KCastro-r <k.fernanda_07@yahoo.com.br>
2026-08-06 10:19:12 -0300

    Resolução Atividade 11

---
+++ Atividade11/Program.cs
@@ -0,0 +1,22 @@
+﻿/*11. Soma até digitar zero
+Leia números inteiros.
+Quando o usuário digitar zero, encerre o programa mostrando a soma dos valores digitados.*/
+
+Console.WriteLine("------------- SOMA ATÉ DIGITAR ZERO -------------");
+
+int num;
+int soma=0;
+
+do
+{
+Console.WriteLine("Digite o número que deseja incluir na soma:");  
+while(!int.TryParse(Console.ReadLine(), out num))
+    {
+       Console.WriteLine("Entrada inválida. Digite um número inteiro:");
+    }
+soma=soma+num;
+
+}
+while(num!= 0);
+
+Console.WriteLine($"A soma dos valores é {soma}");
