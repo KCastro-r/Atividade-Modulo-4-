48ea00546fa586759d7a4a6956dda5ce410ccdf6
KCastro-r <k.fernanda_07@yahoo.com.br>
2026-08-06 14:52:01 -0300

    Resolução Atividade 16

---
+++ Atividade16/Program.cs
@@ -0,0 +1,22 @@
+﻿/*16. Contagem de pares
+Leia 20 números e informe quantos são pares.*/
+
+Console.WriteLine("------------- CONTAGEM DE PARES ------------- ");
+int num;
+int i;
+int quantPares=0;
+
+for (i=1; i<=20; i++)
+{
+     Console.WriteLine($"Digite o {i}º número:");
+
+    while (!int.TryParse(Console.ReadLine(), out num))
+    {
+        Console.WriteLine("Entrada inválida. Digite um número valido:");
+    }
+    if (num%2==0)
+    {
+        quantPares=quantPares+1;
+    }
+}
+Console.WriteLine($"A quantidade Pares informado é: {quantPares}");
