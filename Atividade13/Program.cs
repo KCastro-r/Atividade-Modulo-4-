d960ca49cbfa9fc7c0ef1f31cdf5723ff9aa1934
KCastro-r <k.fernanda_07@yahoo.com.br>
2026-08-06 11:11:32 -0300

    Resolução Atividade 13

---
+++ Atividade13/Program.cs
@@ -0,0 +1,23 @@
+﻿/*13. Verificador de senha
+Solicite uma senha.
+Informe se ela possui pelo menos 8 caracteres.*/
+
+Console.WriteLine("---------- VERIFICADOR DE SENHA ----------");
+
+string? senha;
+int quantidadeCaracter;
+
+Console.WriteLine("Digite a senha:");
+senha=Console.ReadLine();
+if ((senha!="") && (senha!=null))
+{
+ quantidadeCaracter=senha.Length;
+if (quantidadeCaracter>=8)
+{
+   Console.WriteLine("Senha Válida pois ela possui pelo menos 8 caracteres.");
+} 
+else
+{
+    Console.WriteLine("Senha inVálida pois ela não possui pelo menos 8 caracteres.");
+}  
+}
